ee332f8beae15a9712c06d89c7d0a78bf1803628
Luiz Mantuano <luguto23@hotmail.com>
2023-12-06 19:32:19 -0300

    feat:general update

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -7,11 +7,15 @@ import React from 'react';
 import App from '../App';
 
 // Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
+import {it, expect} from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   renderer.create(<App />);
+});
+
+it('should sum correct', () => {
+  expect(2 + 2).toBe(3);
 });
