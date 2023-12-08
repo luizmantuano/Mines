5ce0871d23a9ccb041e726948c63d56b769fc2bf
Luiz Mantuano <luguto23@hotmail.com>
2023-12-07 22:28:56 -0300

    feat: Feat: "upgrade params.js"

---
+++ src/params.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const params = () => {
+  return (
+    <View>
+      <Text>params</Text>
+    </View>
+  )
+}
+
+export default params
