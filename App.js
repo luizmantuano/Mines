aaf3631e25d50463c8492e792f4ab73e5bdad2d9
Luiz Mantuano <luguto23@hotmail.com>
2023-12-09 13:46:39 -0300

    feat: upgrade

---
+++ App.js
@@ -0,0 +1,32 @@
+import {View, Text, StyleSheet} from 'react-native';
+import React, {Component} from 'react';
+
+import params from './src/params';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Iniciando Mines</Text>
+        <Text style={styles.instructions}>
+          Tamanho da grade:
+          {params.getRowsAmount()}x{params.getColumnsAmount()}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
