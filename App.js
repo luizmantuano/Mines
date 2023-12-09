e864d27eb35a4bd3903084dd343e34c4303b8390
Luiz Mantuano <luguto23@hotmail.com>
2023-12-09 14:22:26 -0300

    Feat: "Upgrade in App.js"

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import {View, Text, StyleSheet} from 'react-native';
 import React, {Component} from 'react';
 
 import params from './src/params';
+import Field from './src/components/Field';
 
 export default class App extends Component {
   render() {
@@ -12,6 +13,14 @@ export default class App extends Component {
           Tamanho da grade:
           {params.getRowsAmount()}x{params.getColumnsAmount()}
         </Text>
+        <Field />
+        <Field opened />
+        <Field opened nearMines={1}/>
+        <Field opened nearMines={2}/>
+        <Field opened nearMines={3}/>
+        <Field opened nearMines={6}/>
+        
+        
       </View>
     );
   }
