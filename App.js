96fbabe59715bdaf8d48622535a438565a76968c
Luiz Mantuano <luguto23@hotmail.com>
2023-12-12 12:51:36 -0300

    feat: "Upgrade"

--- App.js
+++ App.js
@@ -2,9 +2,29 @@ import {View, Text, StyleSheet} from 'react-native';
 import React, {Component} from 'react';
 
 import params from './src/params';
-import Field from './src/components/Field';
+import MineField from './src/components/MineField';
+import {createMinedBoard} from './src/functions';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = this.createState();
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
+    return Math.ceil(cols * rows * params.difficultLevel);
+  };
+
+  createState = () => {
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
+    return {
+      board: createMinedBoard(rows, cols, this.minesAmount()),
+    };
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -13,19 +33,9 @@ export default class App extends Component {
           Tamanho da grade:
           {params.getRowsAmount()}x{params.getColumnsAmount()}
         </Text>
-        <Field />
-        <Field opened />
-        <Field opened nearMines={1}/>
-        <Field opened nearMines={2}/>
-        <Field opened nearMines={3}/>
-        <Field opened nearMines={6}/>
-        <Field mined/>
-        <Field mined opened/>
-        <Field mined opened exploded/>
-        <Field flagged/>
-        <Field flagged opened/>
-        
-        
+        <View style={styles.board}>
+          <MineField board={this.state.board}/>
+        </View>
       </View>
     );
   }
@@ -34,13 +44,10 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'flex-end',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  board: {
+    alignItems: 'center',
+    backgroundColor: '#AAA',
   },
 });
